a2127077dcf9f02db95e0144523243f6319584dc
FOREVER-PC <pettkevinj@gmail.com>
2024-06-19 23:42:21 -0600

    Finished Final Project

---
+++ Final Project/conclusions.md
@@ -0,0 +1,15 @@
+*Disclaimer: This conclusion is based on the figures given by the Bureau of Labor Statistic's website for 1961 to 2012 which were obtained on June 17, 2024. These figures are different than the ones provided in Canvas. If these differences change any part of the conclusion, it will be noted.*
+
+Question: Did Bill Clinton have his facts right?
+Answer: Short answer - *no.
+Long answer - Clinton claimed that when Republicans controlled the White House, 24 million private sector jobs were produced. When Democrats controlled the White House, he claims that 42 million of these jobs were produced for a total of 66 million produced jobs. When solving for these totals with the BLS's own data, I found there were 32.677 million produced private sector jobs while the Republicans controlled the White House and 36.49 million produced private sector jobs while the Democrats controlled the White House for a total of 69.167 million.
+*= In the data provided by the BLS_private.csv file from Canvas, his total produced private jobs claim was within 1 million jobs of that data. One could argue that this particular figure is accurate enough. His partisian figures are still incorrect, however.
+
+Clarifying question: Was Clinton's assertion that the private job market does better under Democrat presidencies than Republican presidencies substantiated?
+Answer: If we assume that the United States presidency can have a significant affect on how well the private economy does, then the simple answer is yes. Compared to his claims, the results from the data are closer to parity, only seperated by less than 4 million jobs if using the data directly from the BLS and less than 1 million jobs if using the data from Canvas, both in the Democrats' favor. The strength of Clinton's claim is significantly harmed, however.
+
+Bonus question 1: In what ways is this the wrong data for the context in which it was being used?
+Answer: Job creation is only one indicator of a country's economic health, and it's hard to directly attribute a US president's policies to private sector job creation. Unemployment rate, Gross Domestic Product, inflation rate, federal budget deficits, and the federal reserve interest rates are just a few other indicators of economic health, all with various levels of influence from the US presidency. Clinton implied that America's economy is better off with the Democrats in the White House by referencing job creation, when in reality, the President is incapable of directly forcing private employers to hire additional employees.
+
+Bonus question 2: How could the numbers be adjusted to better reflect the influence that a president has on this data?
+Answer: Signed laws and executive orders and then noting jobs numbers directly after these acts take effect can be one way to attribute a particular president's influence on job creation. Even in this case, indicating which party controls congress when a law improves job creation may make a stronger point.
--- Final Project/fact_check.py
+++ Final Project/fact_check.py
@@ -1,54 +1,132 @@
 import csv
 import sys
-
+import itertools
 
 def main():
+    '''Opens two CSV files: first is a user-specified CSV file which expects
+    data from the Bureau of Labor Statistics, second is data on which political
+    party occupied the White House that month (or the majority of that month).
+    Data is taken from both CSV files and organized into initialized
+    dictionaries for later calculations.
+
+    Job growth by party is calculated by calling the below functions.
+    These calculations are placed into variables and printed into
+    informative data for the end-user.'''
+
+    while True:
+        try:
+            user_data = sys.argv[1]
+            print("Running script.\n")
+            break
+        except IndexError:
+            sys.exit('''Please include a CSV file with labor data such as
+<BLS_private.csv> that contains expected formatting.''')
 
     jobs_data = {}
-    pres_data = {}
-    r_jobs = []
-    d_jobs = []
+    party_data = {}
 
-    with open('BLS_private.csv', newline='') as jobscsv:
-        raw_data = csv.reader(jobscsv, skipinitialspace=True)
-        for iter in range(6):
-            next(raw_data)
-        for row in raw_data:
-            output = [int(ele) for ele in row]
-            jobs_data[output[0]] = output[1:]
-            
+    try:
+        with open(user_data, newline='') as jobscsv:
+            raw_data = csv.reader(jobscsv, skipinitialspace=True)
+            for ele in range(6):
+                next(raw_data)
+            for row in raw_data:
+                output = [int(ele) for ele in row]
+                jobs_data[output[0]] = output[1:]
+    except FileNotFoundError:
+        sys.exit('''The specified file was not found.
+Double check the spelling of the file's name and try again.''')
+    except ValueError:
+        sys.exit('''
+An error occurred while reading the specified CSV jobs data file.
+The specified jobs CSV file has an unexpected format.
+Make sure the file follows the format used by bls.gov's exported data.''')
+
     with open('presidents.csv', newline='') as pres_csv:
         presidents = csv.reader(pres_csv)
         next(presidents)
         for row in presidents:
-            pres_data[int(row[0])] = row[1:]
+            party_data[int(row[0])] = row[1:]
 
-    sort_job_nums(jobs_data, pres_data)
+    t_jobs = job_figures(jobs_data)
+    d_jobs, r_jobs = party_jobs_growth(jobs_data, party_data)
+    d_growth, r_growth, total_growth = growth_by_party(d_jobs, r_jobs, t_jobs)
 
-def sort_job_nums(jobs_dict, terms_dict):
-    jobs = [[ele for ele in jobs_dict[key]] for key in jobs_dict]
-    terms = [[ele for ele in terms_dict[key]] for key in terms_dict]
-    d_jobs= []
-    r_jobs= []
-    for row in terms:
-        row_index = 0
-        print(row)
-        for party in row:
-            inner_index = row.index(party)
-            print(row_index, inner_index, party)
-            if party == 'D':
-                d_jobs.append(jobs[row_index][inner_index])
-            else:
-                r_jobs.append(jobs[row_index][inner_index])
-    #print(r_jobs, d_jobs)
-    #return d_jobs, r_jobs
+    print(f'''
+    Democrats job growth total: {(d_growth * 1000):,d}
+    Clinton's Democratic Party job growth claim: {42000000:,d}\n
+    Republicans job growth total: {(r_growth * 1000):,d}
+    Clinton's Republican Party job growth claim: {24000000:,d}\n
+    Total job growth: {((total_growth) * 1000):,d}
+    Clinton's total job growth claim: {66000000:,d}\n
+    For further analysis, refer to the conclusions.md file.''')
 
-    '''for key, value in jobs_dict.items():
-        for jobs in value:
-            index = value.index(jobs)
-            if terms_dict[key].values():'''
-    
+def job_figures(jobs_dict):
+    '''Returns a 1-D list of all monthly jobs figures
+    from Jan. 1961 to Oct. 2012 in chronological order.'''
 
+    jobs = [list(jobs_dict[key]) for key in jobs_dict]
+    return list(itertools.chain.from_iterable(jobs))
+
+def party_jobs_growth(jobs_data, party_data):
+    '''Returns jobs figures attributed to each political party,
+    organized by consecutive party occupancy of the White House.
+    If party occupancy of the White House changes, the outgoing party is
+    given credit for that month's jobs figure and also passes that figure
+    to the incoming party's jobs data for later growth calculation.'''
+
+    d_jobs = [[]]
+    r_jobs = [[]]
+
+    counter = 0
+    for year, jobs in jobs_data.items():
+        if len(set(party_data[year])) == 2 and party_data[year][0] == 'R':
+            if counter >= 1: #Accounts for edge case with D.D.E.'s final month
+                r_jobs[len(r_jobs) - 1].append(jobs[0])
+                r_jobs += [[]]
+            counter += 1
+            d_jobs[len(d_jobs) - 1] += list(jobs)
+        elif len(set(party_data[year])) == 2 and party_data[year][0] == 'D':
+            d_jobs[len(d_jobs) - 1].append(jobs[0])
+            d_jobs += [[]]
+            r_jobs[len(r_jobs) - 1] += list(jobs)
+        elif len(set(party_data[year])) == 1 and party_data[year][0] == 'R':
+            r_jobs[len(r_jobs) - 1] += list(jobs)
+        else:
+            d_jobs[len(d_jobs) - 1] += list(jobs)
+    return d_jobs, r_jobs
+
+def growth_by_party(d_jobs, r_jobs, t_jobs):
+    '''Returns the total job growth figure by party and total combined.
+    Each instance of consecutive party control of the White House is
+    calculated as its own figure at first and then each of these figures
+    are summed together for that party's total job growth.'''
+
+    d_list = []
+    r_list = []
+
+    for lst in d_jobs:
+        d_list += [recursive_difference(lst)]
+    d_job_growth = sum(d_list)
+
+    for lst in r_jobs:
+        r_list += [recursive_difference(lst)]
+    r_job_growth = sum(r_list)
+
+    total_job_growth = recursive_difference(t_jobs)
+    return d_job_growth, r_job_growth, total_job_growth
+
+def recursive_difference(lst):
+    '''Take a list of numbers, subtract the 2nd number with the 1st,
+    repeat this until the final 2 remaining numbers are subtracted.
+    Return total difference.'''
+
+    if len(lst) <= 1: #Makes function error-resistant to single/empty lists
+        return 0
+    elif len(lst) == 2:
+        return lst[1] - lst[0]
+    else:
+        return lst[1] - lst[0] + recursive_difference(lst[1:])
 
 if __name__ == '__main__':
     main()
